08dddfd521b77add03d17c9b1627a8962f04b6e1
AydanG <aydang@seas.upenn.edu>
2021-09-11 17:23:58 -0400

    not every project too present silly

--- src/components/project_item.jsx
+++ src/components/project_item.jsx
@@ -1,21 +1,12 @@
 import { Tag } from './tag'
 
-export const ProjectItem = ({
-  title,
-  tags,
-  startDate,
-  endDate,
-  content,
-  index,
-}) => {
+export const ProjectItem = ({ title, tags, startDate, content, index }) => {
   return (
     <div className={`project-item delay-animation-${index + 1}`}>
       <div className="header">
         <div>
           <h4>{title}</h4>
-          <small>
-            {startDate} - {endDate ? endDate : `present`}
-          </small>
+          <small>{startDate}</small>
         </div>
         <div
           style={{
